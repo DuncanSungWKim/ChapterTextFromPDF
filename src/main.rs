510c742681b1cf1e20ec8bd7e74954e4a4808b37
Duncan Sung W. Kim <DuncanSungWKim@gmail.com>
2023-11-05 15:16:16 +0900

    Create struct TextExtractor with a file in a folder for saving text

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 use std::{
     env
+,   path::Path, fs, io::prelude::*
 };
 
 
 
 fn main() {
     let fileName = GetPDFFileName() ;
+    let mut textExtractor = TextExtractor::newWithSavingFolder( fileName.as_str() ) ;
     println!("Hello, {}!", fileName );
 }
 
@@ -16,4 +18,33 @@ fn GetPDFFileName()
 {
     let arg1 = env::args().nth( 1 ) ;
     arg1.expect( "PDF file as first argument" )
+}
+
+
+
+struct TextExtractor
+{
+    folderName : String
+,   file : fs::File
+}
+
+
+impl TextExtractor
+{
+    fn newWithSavingFolder(
+        fileName : &str
+    )-> Self
+    {
+        let folderName = Path::new( fileName ).file_stem().unwrap() ;
+        let folder = Path::new( folderName ) ;
+        if folder.is_dir() {
+            fs::remove_dir_all( folder ) ;
+        }
+        fs::create_dir( folder ) ;
+        let filePath = folder.join( "00.txt" ) ;
+        Self {
+            folderName : folderName.to_str().unwrap().to_owned()
+        ,   file : fs::File::create( &filePath ).unwrap()
+        }
+    }
 }
